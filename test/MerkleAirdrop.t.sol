34ec093663c6b106086f75af9804f5f06f76c0ab
Aditya41205 <157035985+Aditya41205@users.noreply.github.com>
2025-03-27 23:01:12 +0530

    updated

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -2,10 +2,30 @@
 pragma solidity ^0.8.20;
 
 
-import {Test} from "forge-std/Test.sol";
+import {Test,console} from "forge-std/Test.sol";
+import {MerkelAirdrop} from "../src/MerkelAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
 
 
 contract MerkleAirdropTest is Test{
-    
+    MerkelAirdrop public Airdrop;
+    BagelToken public token;
+    bytes32 public merkleRoot = 0xfba4401795c371e44a8b8932031439edf0627655a1e69b8eeb1edc10048b9461;
+    uint256 public amounttoclaim = 25* 1e18;
+    bytes32[] public proof = [];
+    address user;
+    uint256 privKey;
+
+    function setUp() public {
+        token = new BagelToken();
+        
+        Airdrop = new MerkelAirdrop(merkleRoot, token);
+        (user, privKey) = makeAddrAndKey("user");
+    }
+    function TestUsersCanClaimAirdrop() public{
+       uint256 startingbalance= token.balanceOf(user);
+       vm.prank(user);
+       airdrop.claim(user, amounttoclaim, proof);
+    }
 }
 
